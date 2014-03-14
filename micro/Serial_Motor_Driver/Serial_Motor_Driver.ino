e18682d159dde45629c369e8814acac99a60135c
Ric <thericcer@gmail.com>
2014-03-14 00:32:08 -0600

    Redid Indenting on Micro Code

--- micro/Serial_Motor_Driver/Serial_Motor_Driver.ino
+++ micro/Serial_Motor_Driver/Serial_Motor_Driver.ino
@@ -134,152 +134,152 @@ void loop(){
     switch (inPacket[0]){
       
       //Drive motors called
-      case DRIVE: 
+    case DRIVE: 
       
-        MotorArray[0] = inPacket[1];
-        MotorArray[1] = inPacket[2];
-        MotorArray[2] = inPacket[3];
-        MotorArray[3] = inPacket[4];
-        
-        //Set Direction, Assuming we want both front and back going the same way
-        if (MotorArray[2] == FORWARD){
-          digitalWrite(2, HIGH);
-          digitalWrite(3, LOW);
-          
-        }
-        else {
-          digitalWrite(2, LOW);
-          digitalWrite(3, HIGH);
-          
-        }
-        
-        if (MotorArray[3] == FORWARD){
-          digitalWrite(4, HIGH);
-          digitalWrite(5, LOW);
-        }
-        
-        else {
-          digitalWrite(4, LOW);
-          digitalWrite(5, HIGH);
-        }
-        
-        
-        //Next set speed
-        analogWrite(11, MotorArray[0]);
-        analogWrite(12, MotorArray[1]);
-
-        break;
-        
-      case STEER:
+      MotorArray[0] = inPacket[1];
+      MotorArray[1] = inPacket[2];
+      MotorArray[2] = inPacket[3];
+      MotorArray[3] = inPacket[4];
       
-        SteerArray[0] = inPacket[1];
-        SteerArray[1] = inPacket[2];
-        SteerArray[2] = inPacket[3];
-        SteerArray[3] = inPacket[4];
-        //Check and set servo angles
-        if(SteerArray[0] <= 180){
-            leftFront.write(SteerArray[0]);
-        }
-        else{
-            StatusByte = 0x4 | StatusByte;
-        }
+      //Set Direction, Assuming we want both front and back going the same way
+      if (MotorArray[2] == FORWARD){
+	digitalWrite(2, HIGH);
+	digitalWrite(3, LOW);
         
-        if(SteerArray[1] <= 180){
-            leftRear.write(SteerArray[1]);
-        }
-        else{
-            StatusByte = 0x4 | StatusByte;
-        }
+      }
+      else {
+	digitalWrite(2, LOW);
+	digitalWrite(3, HIGH);
         
-        if(SteerArray[2] <= 180){
-            rightFront.write(SteerArray[2]);
-        }
-        else{
-            StatusByte = 0x4 | StatusByte;
-        }
-        
-        if(SteerArray[3] <= 180){
-            rightRear.write(SteerArray[3]);
-        }
-        else{
-            StatusByte = 0x4 | StatusByte;
-        }        
-        break;
-        
-        
-      case SENSOR:
-        switch (inPacket[1]){
-          case 0:
-            SensorArray[0] = analogRead(0);
-            break;
-  
-          case 1:
-            SensorArray[1] = analogRead(1);
-            break;
-
-          case 2:
-            pinMode(22, OUTPUT);
-            digitalWrite(22, LOW);
-            delayMicroseconds(2);
-            digitalWrite(22, HIGH);
-            delayMicroseconds(5);
-            digitalWrite(22, LOW);
-            pinMode(22, INPUT);    
-            SensorArray[2] = (pulseIn(22, HIGH, 10000));
-            break;
-          
-  
-          case 3:
-            pinMode(23, OUTPUT);
-            digitalWrite(23, LOW);
-            delayMicroseconds(2);
-            digitalWrite(23, HIGH);
-            delayMicroseconds(5);
-            digitalWrite(23, LOW);
-            pinMode(23, INPUT);    
-            SensorArray[3] = (pulseIn(23, HIGH, 10000));
-            break;
-          
-        }
-        
-        Serial.write(SensorArray[inPacket[1]] & 0xFF);
-        Serial.write((SensorArray[inPacket[1]] >> 8) & 0xFF);
-        break;
+      }
       
-      case STATUS:
-        Serial.write(MotorArray, 4);
-        Serial.write(SteerArray, 4);
-        Serial.write(loopTime & 0xFF);
-        Serial.write((loopTime >> 8) & 0xFF);
+      if (MotorArray[3] == FORWARD){
+	digitalWrite(4, HIGH);
+	digitalWrite(5, LOW);
+      }
+      
+      else {
+	digitalWrite(4, LOW);
+	digitalWrite(5, HIGH);
+      }
+      
+      
+      //Next set speed
+      analogWrite(11, MotorArray[0]);
+      analogWrite(12, MotorArray[1]);
+      
+      break;
+      
+    case STEER:
+      
+      SteerArray[0] = inPacket[1];
+      SteerArray[1] = inPacket[2];
+      SteerArray[2] = inPacket[3];
+      SteerArray[3] = inPacket[4];
+      //Check and set servo angles
+      if(SteerArray[0] <= 180){
+	leftFront.write(SteerArray[0]);
+      }
+      else{
+	StatusByte = 0x4 | StatusByte;
+      }
+      
+      if(SteerArray[1] <= 180){
+	leftRear.write(SteerArray[1]);
+      }
+      else{
+	StatusByte = 0x4 | StatusByte;
+      }
+      
+      if(SteerArray[2] <= 180){
+	rightFront.write(SteerArray[2]);
+      }
+      else{
+	StatusByte = 0x4 | StatusByte;
+      }
+      
+      if(SteerArray[3] <= 180){
+	rightRear.write(SteerArray[3]);
+      }
+      else{
+	StatusByte = 0x4 | StatusByte;
+      }        
+      break;
+      
+      
+    case SENSOR:
+      switch (inPacket[1]){
+      case 0:
+	SensorArray[0] = analogRead(0);
+	break;
+	
+      case 1:
+	SensorArray[1] = analogRead(1);
+	break;
+	
+      case 2:
+	pinMode(22, OUTPUT);
+	digitalWrite(22, LOW);
+	delayMicroseconds(2);
+	digitalWrite(22, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(22, LOW);
+	pinMode(22, INPUT);    
+	SensorArray[2] = (pulseIn(22, HIGH, 10000));
+	break;
         
-        break;
+	
+      case 3:
+	pinMode(23, OUTPUT);
+	digitalWrite(23, LOW);
+	delayMicroseconds(2);
+	digitalWrite(23, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(23, LOW);
+	pinMode(23, INPUT);    
+	SensorArray[3] = (pulseIn(23, HIGH, 10000));
+	break;
+        
+      }
+      
+      Serial.write(SensorArray[inPacket[1]] & 0xFF);
+      Serial.write((SensorArray[inPacket[1]] >> 8) & 0xFF);
+      break;
+      
+    case STATUS:
+      Serial.write(MotorArray, 4);
+      Serial.write(SteerArray, 4);
+      Serial.write(loopTime & 0xFF);
+      Serial.write((loopTime >> 8) & 0xFF);
+      
+      break;
       
       //Platform packet received, drive platform up or down
-      case PLATFORM:
-        if(inPacket[1] == 'U'){
-            //Move Platform Up
-            platformRight.write(180);
-            platformLeft.write(180);
-        }
-        if(inPacket[1] == 'D'){
-            //Move Platform Down
-            platformRight.write(0);
-            platformLeft.write(0);
-        }
-        if(inPacket[1] == 'S'){
-            platformRight.write(90);
-            platformLeft.write(90);
-        }
-        break;
-        
-      default:
-        break;
+    case PLATFORM:
+      if(inPacket[1] == 'U'){
+	//Move Platform Up
+	platformRight.write(180);
+	platformLeft.write(180);
+      }
+      if(inPacket[1] == 'D'){
+	//Move Platform Down
+	platformRight.write(0);
+	platformLeft.write(0);
+      }
+      if(inPacket[1] == 'S'){
+	platformRight.write(90);
+	platformLeft.write(90);
+      }
+      break;
+      
+    default:
+      break;
     }
   }
   
   loopTime = millis() - oldLoopTime;  
   LoopCount++;
 }
-  
+
 
 
