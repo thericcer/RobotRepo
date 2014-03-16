d7d97519ab42c958185eb5dda836dc78f2be9cfa
Ric <thericcer@gmail.com>
2014-03-15 21:24:55 -0600

    Finished wall follow code. Works like the python version.

--- wallFollow/wallFollow.cpp
+++ wallFollow/wallFollow.cpp
@@ -14,19 +14,19 @@ void setDistance(void){
   controller.getSensor(2, &sensors[2]);
   controller.getSensor(3, &sensors[3]);
 
-    if(sensors[2] > 1000){
+    if(sensors[2] > 1100){
     controller.steer(0, 0, 0, 0);
     controller.drive(150, 150, 'F', 'F');
     while(sensors[2] > 1000){
       controller.getSensor(2, &sensors[2]);
-      printf("Moving Closer to wall: %10d\n", sensors[2]);
+      printf("Moving Away from wall: %10d\n", sensors[2]);
     }
     usleep(100000);
     controller.steer(90, 90, 90, 90);
     controller.drive(0, 0, 'F', 'F');
   }
   
-  if(sensors[2] < 700){
+  if(sensors[2] < 1000){
     controller.steer(0, 0, 0, 0);
     controller.drive(150, 150, 'R', 'R');
     while(sensors[2] < 700){
@@ -54,10 +54,10 @@ void getSquare(void){
     printf("Angle %5f | Sensor1: %10d | Sensor2 %10d\n", angle, sensors[2], sensors[3]);
     
 
-    if(angle > 0.5){
-      controller.steer(135, 45, 135, 45);
-      controller.drive(150, 150, 'R', 'F');
-      while(angle > 0.5){
+    if(angle > 0.05){
+      controller.steer(45,135, 45, 135);
+      controller.drive(150, 150, 'F', 'R');
+      while(angle > 0.05){
 	controller.getSensor(2, &sensors[2]);
 	controller.getSensor(3, &sensors[3]);
 	angle = atan2((double)sensors[2]-(double)sensors[3], 900);
@@ -67,10 +67,10 @@ void getSquare(void){
       controller.steer(90, 90, 90, 90);
     }
 
-    if(angle < -0.5){
-      controller.steer(135, 45, 135, 45);
-      controller.drive(150, 150, 'F', 'R');
-      while(angle < -0.5){
+    if(angle < -0.05){
+      controller.steer(45, 135, 45, 135);
+      controller.drive(150, 150, 'R', 'F');
+      while(angle < -0.05){
 	controller.getSensor(2, &sensors[2]);
 	controller.getSensor(3, &sensors[3]);
 	angle = atan2((double)sensors[2]-(double)sensors[3], 900);
@@ -89,7 +89,12 @@ int main(void){
   getSquare();
   
 
+
   while(1){
+    controller.drive(150, 150, 'F', 'F');
+    sleep(1);
+    setDistance();
+    getSquare();
 
   }
   return 0;
