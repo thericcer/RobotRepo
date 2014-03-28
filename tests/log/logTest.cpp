4b77b796f90726a5d47dcafa1771805fedd9c364
odroid@odroid.com <odroid@odroid.com>
2014-03-27 21:15:30 -0700

    logging stuff

--- tests/log/logTest.cpp
+++ tests/log/logTest.cpp
@@ -7,15 +7,15 @@ int main(void){
 
   openlog("Robot", LOG_PERROR | LOG_CONS | LOG_NDELAY, LOG_LOCAL0);
 
-  syslog(LOG_MAKEPRI(LOG_LOCAL0, LOG_INFO), "INFO");
+  syslog(LOG_MAKEPRI(LOG_LOCAL0, LOG_INFO), "INFO this is just some random info");
 
   sleep(5);
   
-  syslog(LOG_MAKEPRI(LOG_LOCAL0, LOG_WARNING), "Warning");
+  syslog(LOG_MAKEPRI(LOG_LOCAL0, LOG_WARNING), "Warning, a warning from the bot");
 
   sleep(5);
   
-  syslog(LOG_MAKEPRI(LOG_LOCAL0, LOG_ERR), "Super hello!");
+  syslog(LOG_MAKEPRI(LOG_LOCAL0, LOG_ERR), "Super hello! %d", 23);
 
   closelog();
 }
